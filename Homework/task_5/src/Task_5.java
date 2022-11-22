d3e726493a79d1966096274e4c423b4c0c916785
reddraw <112946235+reddraw@users.noreply.github.com>
2022-11-22 16:01:35 +0300

    completed task 5

---
+++ Homework/task_5/src/Task_5.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Task_5 implements Task_5_base {
+    @Override
+    public ArrayList<Integer> subtask_1_ArrayList(ArrayList<Integer> data, int k, int n) {
+        // Выбрать из данного списка элементы, кратные k, но не кратные n
+        // и сформировать из них новый список.
+        // Вернуть новый список в качестве результата
+        ArrayList<Integer> bebra = new ArrayList<>();
+        for (int i = 0; i < data.size(); ++i) {
+            if (data.get(i) % k == 0 && data.get(i) % n != 0) {
+                bebra.add(data.get(i));
+            }
+        }
+        return bebra;
+    }
+
+    @Override
+    public ArrayList<Integer> subtask_2_ArrayList(int size) {
+        // сгенерировать и вернуть список размера size,
+        // содержащий первые size элементов последовательности, описанной в
+        // задаче subtask_2_for задания task_3
+        ArrayList<Integer> bebra = new ArrayList<>();
+        for (int i = 0, k = 0; k < size; ++i) {
+            for (int a = 0; k < size && a < i; ++a, ++k) {
+                bebra.add(i);
+            }
+        }
+        return bebra;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_3_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // найдите пересечение множеств s1 и s2
+        s1.retainAll(s2);
+        return s1;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_4_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // найдите объединение множеств s1 и s2
+        s1.addAll(s2);
+        return s1;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_5_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // найдите дополнение множества s1 до множества s2
+        s2.removeAll(s1);
+        return s2;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_6_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // постройте множество, содержащее элементы, содержащиеся либо только в s1,
+        // либо только в s2, но не в обоих множествах одновременно
+        HashSet<Integer> bebra = new HashSet<>(s1);
+        HashSet<Integer> bebra1 = new HashSet<>(s2);
+        s2.removeAll(bebra);
+        s1.removeAll(bebra1);
+        HashSet<Integer> prikol = new HashSet<>(s1);
+        prikol.addAll(s2);
+        return prikol;
+    }
+
+    @Override
+    public HashMap<String, Double> subtask_7_HashMap(ArrayList<String> data) {
+        // Дан список строк. Построить словарь, содержащий частоты слов
+        // для данного списка в процентах
+		return null;
+    }
+
+    @Override
+    public HashMap<String, Double> subtask_8_HashMap(ArrayList<Double> data) {
+        // Дан список чисел. Сформировать словарь, содержащий среднее,
+        // максимальное и минимальное значения из данного списка. Ключи
+        // словаря "mean", "max", "min" соответственно
+        return null;
+    }
+}
